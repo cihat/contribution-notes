ad3bc066e4e978c6598e52e8792343621f37b5d5
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-09-07 10:56:08 +0300

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const COLOR_MAP = {
+  0: "#ebedf0",
+  1: "#9be9a8",
+  2: "#40c463",
+  3: "#30a14e",
+  4: "#216e39"
+};
+
+export const username = "cihat"
+export const format = "nested"
