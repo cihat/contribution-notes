8f0db431c83af1c39997225827182ca6273fc16b
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-09-07 12:50:30 +0300

    Add `shadcn` ui lib

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script>
 	import { onMount } from 'svelte';
+	import * as Resizable from '../lib/components/ui/resizable/index.ts';
 
 	let username = 'cihat';
 	const endpoint = '/github';
 	async function getData() {
 		console.log(username);
-		fetch(endpoint, {
-			// method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		})
+		fetch(endpoint)
 			.then((res) => res.json())
 			.then((data) => console.log(data))
 			.catch((err) => console.log(err));
@@ -22,4 +18,17 @@
 </script>
 
 <main>
+	<Resizable.PaneGroup direction="horizontal" class="!h-screen border">
+		<Resizable.Pane defaultSize={25}>
+			<div class="flex h-full items-center justify-center p-6">
+				<span class="font-semibold">Sidebar</span>
+			</div>
+		</Resizable.Pane>
+		<Resizable.Handle withHandle />
+		<Resizable.Pane defaultSize={75}>
+			<div class="flex h-full items-center justify-center p-6">
+				<span class="font-semibold">Content</span>
+			</div>
+		</Resizable.Pane>
+	</Resizable.PaneGroup>
 </main>
