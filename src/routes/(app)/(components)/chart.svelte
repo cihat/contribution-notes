d150b231025c19acc84407ff6c5466536c0cf5ca
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-09-10 23:06:30 +0300

    Refactor chart component to svelte's reactive state with `$: {...}`

--- src/routes/(app)/(components)/chart.svelte
+++ src/routes/(app)/(components)/chart.svelte
@@ -1,55 +1,62 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
 	import { toast } from 'svelte-sonner';
 	import ContributionsCanvas from './ContributionsCanvas/contributions-canvas.svelte';
 	import { userStore } from '~/store';
 	import { Status, type State } from '~/types';
 
-	let store: State;
-	let options = null;
+	type Options = {
+		data: any;
+		username: string;
+		themeName: string;
+		footerText: string;
+	};
 
-	const unsubscribe = userStore.subscribe(async (value) => {
-		store = value;
-		if (store.status.type === Status.Success && store.userContributions) {
+	let options: Options = {
+		data: null,
+		username: '',
+		themeName: 'standard',
+		footerText: 'GitHub Contributions Chart'
+	};
+
+	$: {
+		if ($userStore.status.type === Status.Success && $userStore.userContributions) {
+			console.log('store >>', $userStore);
+
 			options = {
-				data: store.userContributions,
-				username: store.userName,
+				data: $userStore.userContributions,
+				username: $userStore.userName,
 				themeName: 'standard',
 				footerText: 'GitHub Contributions Chart'
 			};
-		} else if (store.status.type === Status.Error) {
+		} else {
 			toast.error("Chart couldn't be drawn!", {
 				description: 'User contributions not found!'
 			});
 		}
-	});
-
-	onDestroy(() => {
-		unsubscribe();
-	});
+	}
 </script>
 
 <div class="no-scrollbar chart-wrapper flex min-h-screen justify-center overflow-auto">
-	{#if store.status.type === Status.Success && options}
+	{#if $userStore.status.type === Status.Success && options}
 		<ContributionsCanvas {options} />
 	{:else}
 		<div
 			class="absolute top-40 z-40 flex scroll-m-20 rounded-md border bg-white p-20 text-xl font-semibold tracking-tight md:top-20"
 		>
-			{#if store.status.type === Status.Loading}
+			{#if $userStore.status.type === Status.Loading}
 				<div class="flex items-center justify-center">
 					<p class="text-center align-middle text-xl font-bold text-blue-500">Loading canvas...</p>
 				</div>
-			{:else if store.status.type === Status.Idle}
+			{:else if $userStore.status.type === Status.Idle}
 				<div class="items center flex items-center justify-center">
 					<p class="text-center align-middle text-xl font-bold text-orange-500">
 						Search for a user to draw chart!
 					</p>
 				</div>
-			{:else if store.status.type === Status.Error}
+			{:else if $userStore.status.type === Status.Error}
 				<div class="flex items-center justify-center">
 					<p class="block text-center align-middle text-xl text-red-400">
-						{store.status.message}
+						{$userStore.status.message}
 					</p>
 				</div>
 			{/if}
